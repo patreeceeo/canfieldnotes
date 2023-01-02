a1476e00b638cff4fc5f52dc4ddbef2043f09bf7
Patrick Canfield <pscale01@gmail.com>
2023-01-02 08:26:19 -0800

    set alphas

--- src/functions/ColorFunctions.ts
+++ src/functions/ColorFunctions.ts
@@ -7,9 +7,9 @@ export function setPrimaryColor() {
   const hueValue = (((getDayOfYear(now) / 366) + 0.5) % 1) * 360
   const isLightOut = (hours > 6) && (hours < 18)
   const colorPrimary = new Color().hsl(hueValue, 80, isLightOut ? 80 : 20)
-  const colorText = new Color().hsl(0, 0, isLightOut ? 0 : 100)
-  const colorShadow = new Color().hsl(0, 0, isLightOut ? 100 : 0)
+  const colorText = new Color().hsl(0, 0, isLightOut ? 0 : 100).alpha(0.8)
+  const colorShadow = new Color().hsl(0, 0, isLightOut ? 100 : 0).alpha(0.8)
   document.documentElement.style.setProperty('--color-primary', colorPrimary.hex())
-  document.documentElement.style.setProperty('--color-text', colorText.hex())
-  document.documentElement.style.setProperty('--color-shadow', colorShadow.hex())
+  document.documentElement.style.setProperty('--color-text', colorText.hexa())
+  document.documentElement.style.setProperty('--color-shadow', colorShadow.hexa())
 }
