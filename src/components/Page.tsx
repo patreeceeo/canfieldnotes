c19cde54d3a01022bd68595bc9ebad95d7f467c9
Patrick Canfield <pscale01@gmail.com>
2023-01-04 10:07:22 -0800

    make text easier to read on mobile

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -2,7 +2,7 @@ import "./Page.css";
 
 export default function Page(props) {
   return (
-    <main class={`Page ${props.withHeader ? "Page--withHeader" : ""}`}>
+    <main class={`Page mw-100 m-1-auto p-0-1 ${props.withHeader ? "Page--withHeader" : ""}`}>
       {props.children}
     </main>
   );
